6d4c2f157f0c0008e0947c48cbd1a7df1f80aa89
Gucci-Goku <st10091204@vcconnect.edu.za>
2023-04-25 09:39:48 +0200

    Local Repository

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+using APIDemo.Models;
+
+namespace APIDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        [Route("Product")]// for this method to be executed, the URL will be the name of the controller and the name of the route which is product
+        public string returnProduct()
+        {
+           
+
+            return "Eggs";
+        }
+
+        [HttpGet]
+        [Route("All")]
+        public IEnumerable<Product> GetProducts()
+        {
+            return new List<Product>
+            {
+                new Product(1,"Coke"),
+                new Product(2,"Red Bull"),
+                new Product(4,"Prime"),
+                new Product(6,"Bread"),
+                new Product(8,"Chicken"),
+                new Product(10,"Fruit"),
+            };
+        }
+
+
+
+    }
+}
